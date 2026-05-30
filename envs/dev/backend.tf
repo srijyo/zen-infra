55189db7728668f2a6a182595a78a30386e129a2
Ranjini Ganeshan <32661402+ranjiniganeshan@users.noreply.github.com>
2026-05-30 19:38:56 +0530

    Update backend.tf

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -4,7 +4,9 @@ terraform {
   backend "s3" {
     bucket = "zen-pharma-terraform-state-dpp-2026"
     key    = "envs/dev/terraform.tfstate"
+    region = "us-east-1"
     encrypt      = true
     use_lockfile = true   # S3 native locking
+
   }
 }
