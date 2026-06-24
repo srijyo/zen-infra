551b8ed698eda80271d69550ad5067e7544b0a04
Ranjini Ganeshan <32661402+ranjiniganeshan@users.noreply.github.com>
2026-06-24 19:03:29 +0530

    Update variables.tf

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -22,7 +22,7 @@ variable "subnet_ids" {
 variable "node_instance_type" {
   description = "EC2 instance type for EKS worker nodes"
   type        = string
-  default     = "t3.medium"
+  default     = "t3.small"
 }
 
 variable "desired_capacity" {
