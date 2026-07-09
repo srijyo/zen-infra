dfb164061436276efef37a4a4fc3a62739b68383
srijyo <srikanthgandharapu4444@gmail.com>
2026-07-09 17:05:26 +0530

    config update backend bucketname for my account

--- envs/qa/backend.tf
+++ envs/qa/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "zen-pharma-terraform-state-ranjiniganeshan"
+    bucket       = "zen-pharma-terraform-state-srijyo"
     key          = "envs/qa/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
